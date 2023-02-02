9d685498b90f6107b7d8af1787177557bedb48b7
root <root@v1641545.hosted-by-vdsina.ru>
2023-02-02 14:47:29 +0000

    first commit

---
+++ reverse_lines.cpp
@@ -0,0 +1,35 @@
+﻿#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Line
+{
+    Line* prev;
+    string line;
+};
+
+int main()
+{
+    Line* last = nullptr;
+
+    // Чтение строк.
+    for (string line; getline(cin, line);)
+    {
+        Line* new_line = new Line;
+        new_line->prev = last;
+        new_line->line = line;
+        last = new_line;
+    }
+
+    // Вывод строк в обратном порядке.
+    while (last)
+    {
+        cout << last->line << '\n';
+        Line* old_line = last;
+        last = last->prev;
+        delete old_line;
+    }
+
+    return EXIT_SUCCESS;
+}
